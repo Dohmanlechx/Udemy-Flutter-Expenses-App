f431e59a8666f09f5623ea00eeb34250b75c7284
David Öhman <dohman_92@hotmail.com>
2020-02-08 14:12:14 +0100

    -

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import './widgets/new_transaction.dart';
@@ -94,31 +97,42 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final mq = MediaQuery.of(context);
+    final isLandscape = mq.orientation == Orientation.landscape;
 
-    final myAppBar = AppBar(
-      title: Text("Personal Expenses"),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.add),
-          onPressed: () => _startAddNewTransaction(context),
-        ),
-      ],
-    );
+    final PreferredSizeWidget myAppBar = Platform.isIOS
+        ? CupertinoNavigationBar(
+            middle: Text("Personal Expenses"),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                GestureDetector(
+                  child: Icon(CupertinoIcons.add),
+                  onTap: () => _startAddNewTransaction(context),
+                )
+              ],
+            ),
+          )
+        : AppBar(
+            title: Text("Personal Expenses"),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () => _startAddNewTransaction(context),
+              ),
+            ],
+          );
 
     final transactionListWidget = Container(
-      height:
-          (MediaQuery.of(context).size.height - myAppBar.preferredSize.height - MediaQuery.of(context).padding.top) *
-              0.7,
+      height: (mq.size.height - myAppBar.preferredSize.height - mq.padding.top) * 0.7,
       child: TransactionList(
         _userTransactions,
         _deleteTransaction,
       ),
     );
 
-    return Scaffold(
-      appBar: myAppBar,
-      body: SingleChildScrollView(
+    final pageBody = SafeArea(
+      child: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -127,8 +141,12 @@ class _MyHomePageState extends State<MyHomePage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text("Show Chart"),
-                  Switch(
+                  Text(
+                    "Show Chart",
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  Switch.adaptive(
+                    activeColor: Theme.of(context).accentColor,
                     value: _showChart,
                     onChanged: (newValue) {
                       setState(() {
@@ -140,31 +158,37 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             if (!isLandscape)
               Container(
-                height: (MediaQuery.of(context).size.height -
-                        myAppBar.preferredSize.height -
-                        MediaQuery.of(context).padding.top) *
-                    0.3,
+                height: (mq.size.height - myAppBar.preferredSize.height - mq.padding.top) * 0.3,
                 child: Chart(_recentTransactions),
               ),
             if (!isLandscape) transactionListWidget,
             if (isLandscape)
               _showChart
                   ? Container(
-                      height: (MediaQuery.of(context).size.height -
-                              myAppBar.preferredSize.height -
-                              MediaQuery.of(context).padding.top) *
-                          0.7,
+                      height: (mq.size.height - myAppBar.preferredSize.height - mq.padding.top) * 0.7,
                       child: Chart(_recentTransactions),
                     )
                   : transactionListWidget,
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => _startAddNewTransaction(context),
-      ),
     );
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            child: pageBody,
+            navigationBar: myAppBar,
+          )
+        : Scaffold(
+            appBar: myAppBar,
+            body: pageBody,
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: Platform.isIOS
+                ? Container()
+                : FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: () => _startAddNewTransaction(context),
+                  ),
+          );
   }
 }
---
+++ lib/widgets/adaptive_button.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AdaptiveButton extends StatelessWidget {
+  final String btnText;
+  final Function onClick;
+
+  const AdaptiveButton({
+    @required this.btnText,
+    @required this.onClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            child: Text(
+              btnText,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: onClick,
+          )
+        : FlatButton(
+            textColor: Theme.of(context).primaryColor,
+            child: Text(
+              btnText,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: onClick,
+          );
+  }
+}
